ff4a81b1912f15676cf9f8b7f335e750914ea557
Nathan M.S <natms24@gmail.com>
2024-09-21 21:20:52 +0300

    Fix interface implementation bugs

--- backend/delivery/routers/auth_router.go
+++ backend/delivery/routers/auth_router.go
@@ -1,12 +1,29 @@
 package routers
 
 import (
+	"learned-api/delivery/controllers"
+	hashing_service "learned-api/infrastructure/hashing"
+	jwt_service "learned-api/infrastructure/jwt"
+	validation_services "learned-api/infrastructure/validation"
+	"learned-api/repository"
+	usecases "learned-api/usecase"
+
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func NewAuthRouter(collection *mongo.Collection, router *gin.RouterGroup) {
 	authRouter := router.Group("/auth")
-	authRouter.POST("/signup", func(c *gin.Context) {})
-	authRouter.POST("/login", func(c *gin.Context) {})
+
+	authRepository := repository.NewAuthRepository(collection)
+	authUsecase := usecases.NewAuthUsecase(
+		authRepository,
+		validation_services.NewAuthValidation(),
+		hashing_service.NewHashingService(),
+		jwt_service.NewJWTService("secret"),
+	)
+	authController := controllers.NewAuthController(authUsecase)
+
+	authRouter.POST("/signup", authController.Signup)
+	authRouter.POST("/login", authController.Login)
 }
--- backend/domain/services.go
+++ backend/domain/services.go
@@ -28,7 +28,7 @@ type AIModelInterface interface {
 
 type AuthValidation interface {
 	ValidateName(name string) CodedError
-	ValidatieEmail(email string) CodedError
+	ValidateEmail(email string) CodedError
 	ValidatePassword(password string) CodedError
 	ValidateType(userType string) CodedError
 	ValidateUser(user User) CodedError
