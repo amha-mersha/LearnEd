a2f207b2dd9db9e6520bf395e666aabb93bdcc99
Nathan M.S <natms24@gmail.com>
2024-09-21 13:04:31 +0300

    Add domain objects for users and constants

--- backend/domain/domain.go
+++ backend/domain/domain.go
@@ -1,1 +1,28 @@
 package domain
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	CollectionUsers = "users"
+)
+
+const (
+	RoleTeacher = "teacher"
+	RoleStudent = "student"
+)
+
+type Response gin.H
+
+type EnvironmentVariables struct {
+	DB_ADDRESS string
+	DB_NAME    string
+}
+
+type User struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	Type     string `json:"type"`
+}
