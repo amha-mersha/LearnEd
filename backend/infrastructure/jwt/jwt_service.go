c8d622a396ce4770beadf299d2a200e6fda53871
Nathan M.S <natms24@gmail.com>
2024-09-21 20:30:45 +0300

    Add auth middleware

--- backend/infrastructure/jwt/jwt_service.go
+++ backend/infrastructure/jwt/jwt_service.go
@@ -16,7 +16,7 @@ func NewJWTService(secret string) *JWTService {
 	return &JWTService{secret: secret}
 }
 
-func (s *JWTService) SignJWTWithPayload(username string, role string, tokenType string, tokenLifeSpan time.Duration) (string, domain.CodedError) {
+func (s *JWTService) SignJWTWithPayload(email string, role string, tokenType string, tokenLifeSpan time.Duration) (string, domain.CodedError) {
 	if s.secret == "" {
 		return "", domain.NewError("internal server error", domain.ERR_INTERNAL_SERVER)
 	}
@@ -27,7 +27,7 @@ func (s *JWTService) SignJWTWithPayload(username string, role string, tokenType 
 
 	jwtSecret := []byte(s.secret)
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"username":  username,
+		"email":     email,
 		"role":      role,
 		"expiresAt": time.Now().Round(0).Add(tokenLifeSpan),
 		"tokenType": tokenType,
@@ -75,13 +75,13 @@ func (s *JWTService) GetExpiryDate(token *jwt.Token) (time.Time, domain.CodedErr
 	return expiresAtTime, nil
 }
 
-func (s *JWTService) GetUsername(token *jwt.Token) (string, domain.CodedError) {
-	username, ok := token.Claims.(jwt.MapClaims)["username"]
+func (s *JWTService) GetEmail(token *jwt.Token) (string, domain.CodedError) {
+	email, ok := token.Claims.(jwt.MapClaims)["email"]
 	if !ok {
-		return "", domain.NewError("Invalid token: Username not found", domain.ERR_UNAUTHORIZED)
+		return "", domain.NewError("Invalid token: Email not found", domain.ERR_UNAUTHORIZED)
 	}
 
-	return fmt.Sprintf("%v", username), nil
+	return fmt.Sprintf("%v", email), nil
 }
 
 func (s *JWTService) GetRole(token *jwt.Token) (string, domain.CodedError) {
---
+++ backend/infrastructure/middleware/auth_middleware.go
@@ -0,0 +1,88 @@
+package middleware
+
+import (
+	"fmt"
+	"learned-api/domain"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetMiddlewareError(c *gin.Context, statusCode int, message string) {
+	c.JSON(statusCode, domain.Response{"message": message})
+	c.Abort()
+}
+
+func AuthMiddlewareWithRoles(jwtService domain.JWTServiceInterface, validRoles ...string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			SetMiddlewareError(c, http.StatusUnauthorized, "Authorization header not found")
+			return
+		}
+
+		headerSegments := strings.Split(authHeader, " ")
+		if len(headerSegments) != 2 || strings.ToLower(headerSegments[0]) != "bearer" {
+			SetMiddlewareError(c, http.StatusUnauthorized, "Authorization header is invalid")
+			return
+		}
+
+		token, validErr := jwtService.ValidateAndParseToken(headerSegments[1])
+		if validErr != nil {
+			SetMiddlewareError(c, http.StatusUnauthorized, validErr.Error())
+			return
+		}
+
+		tokenType, err := jwtService.GetTokenType(token)
+		if err != nil {
+			SetMiddlewareError(c, http.StatusUnauthorized, err.Error())
+			return
+		}
+
+		if tokenType != "accessToken" {
+			SetMiddlewareError(c, http.StatusUnauthorized, "Invalid token type: make sure to use the accessToken to authorize actions")
+			return
+		}
+
+		expiresAtTime, err := jwtService.GetExpiryDate(token)
+		if err != nil {
+			SetMiddlewareError(c, http.StatusUnauthorized, err.Error())
+			return
+		}
+
+		if expiresAtTime.Compare(time.Now()) == -1 {
+			SetMiddlewareError(c, http.StatusUnauthorized, "Token expired")
+			return
+		}
+
+		userRole, err := jwtService.GetRole(token)
+		if err != nil {
+			SetMiddlewareError(c, http.StatusUnauthorized, err.Error())
+			return
+		}
+
+		email, err := jwtService.GetEmail(token)
+		if err != nil {
+			SetMiddlewareError(c, http.StatusUnauthorized, err.Error())
+			return
+		}
+
+		valid := false
+		for _, validRole := range validRoles {
+			if userRole == validRole {
+				valid = true
+				break
+			}
+		}
+
+		if !valid {
+			SetMiddlewareError(c, http.StatusForbidden, fmt.Sprintf("'%v' roles are not allowed to access this endpoint", userRole))
+			return
+		}
+
+		c.Set("email", email)
+		c.Next()
+	}
+}
