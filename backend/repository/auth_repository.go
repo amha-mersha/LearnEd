3e79cc349b08beda4360b697001882bd15b21a40
Nathan M.S <natms24@gmail.com>
2024-09-21 19:20:39 +0300

    Add repository logic for account login

--- backend/repository/auth_repository.go
+++ backend/repository/auth_repository.go
@@ -1,11 +1,11 @@
 package repository
 
 import (
-	"context"
 	"learned-api/domain"
 	"strings"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -18,7 +18,7 @@ func NewAuthRepository(collection *mongo.Collection) *AuthRepository {
 }
 
 func (r *AuthRepository) CreateUser(c *gin.Context, user domain.User) domain.CodedError {
-	_, err := r.collection.InsertOne(context.Background(), user)
+	_, err := r.collection.InsertOne(c, user)
 	if mongo.IsDuplicateKeyError(err) && strings.Contains(err.Error(), "email") {
 		return *domain.NewError("an account with that email already exists", domain.ERR_CONFLICT)
 	}
@@ -28,4 +28,24 @@ func (r *AuthRepository) CreateUser(c *gin.Context, user domain.User) domain.Cod
 	}
 
 	return nil
+}
+
+func (r *AuthRepository) GetUserByEmail(c *gin.Context, email string) (domain.User, domain.CodedError) {
+	var foundUser domain.User
+
+	res := r.collection.FindOne(c, bson.D{{Key: "email", Value: email}})
+	if res.Err() == mongo.ErrNoDocuments {
+		return foundUser, domain.NewError("user not found", domain.ERR_NOT_FOUND)
+	}
+
+	if res.Err() != nil {
+		return foundUser, domain.NewError(res.Err().Error(), domain.ERR_INTERNAL_SERVER)
+	}
+
+	err := res.Decode(&foundUser)
+	if err != nil {
+		return foundUser, domain.NewError(err.Error(), domain.ERR_INTERNAL_SERVER)
+	}
+
+	return foundUser, nil
 }
