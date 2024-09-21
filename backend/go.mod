fa31bbf18024dd03688769defe8a51e4aad4ad3c
Nathan M.S <natms24@gmail.com>
2024-09-21 19:05:34 +0300

    Add jwt service infrastructure

--- backend/go.mod
+++ backend/go.mod
@@ -4,8 +4,10 @@ go 1.22.5
 
 require (
 	github.com/gin-gonic/gin v1.10.0
+	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/joho/godotenv v1.5.1
 	go.mongodb.org/mongo-driver v1.17.0
+	golang.org/x/crypto v0.26.0
 )
 
 require (
@@ -36,7 +38,6 @@ require (
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20240726163527-a2c0da244d78 // indirect
 	golang.org/x/arch v0.8.0 // indirect
-	golang.org/x/crypto v0.26.0 // indirect
 	golang.org/x/net v0.25.0 // indirect
 	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/sys v0.23.0 // indirect
---
+++ backend/infrastructure/jwt/jwt_service.go
@@ -0,0 +1,103 @@
+package jwt_service
+
+import (
+	"fmt"
+	"learned-api/domain"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+)
+
+type JWTService struct {
+	secret string
+}
+
+func NewJWTService(secret string) *JWTService {
+	return &JWTService{secret: secret}
+}
+
+func (s *JWTService) SignJWTWithPayload(username string, role string, tokenType string, tokenLifeSpan time.Duration) (string, domain.CodedError) {
+	if s.secret == "" {
+		return "", domain.NewError("internal server error", domain.ERR_INTERNAL_SERVER)
+	}
+
+	if tokenType != "accessToken" && tokenType != "refreshToken" {
+		return "", domain.NewError("Invalid token type field", domain.ERR_INTERNAL_SERVER)
+	}
+
+	jwtSecret := []byte(s.secret)
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"username":  username,
+		"role":      role,
+		"expiresAt": time.Now().Round(0).Add(tokenLifeSpan),
+		"tokenType": tokenType,
+	})
+	jwtToken, signingErr := token.SignedString(jwtSecret)
+	if signingErr != nil {
+		return "", domain.NewError("internal server error: "+signingErr.Error(), domain.ERR_INTERNAL_SERVER)
+	}
+
+	return jwtToken, nil
+}
+
+func (s *JWTService) ValidateAndParseToken(rawToken string) (*jwt.Token, error) {
+	token, err := jwt.Parse(rawToken, func(t *jwt.Token) (interface{}, error) {
+		_, ok := t.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", t.Header["alg"])
+		}
+
+		return []byte(s.secret), nil
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("error: " + err.Error())
+	}
+
+	if !token.Valid {
+		return nil, fmt.Errorf("error: Invalid token,  Potentially malformed")
+	}
+
+	return token, nil
+}
+
+func (s *JWTService) GetExpiryDate(token *jwt.Token) (time.Time, domain.CodedError) {
+	expiresAt, ok := token.Claims.(jwt.MapClaims)["expiresAt"]
+	if !ok {
+		return time.Now(), domain.NewError("Invalid token: Expiry date not found", domain.ERR_UNAUTHORIZED)
+	}
+
+	expiresAtTime, convErr := time.Parse(time.RFC3339Nano, fmt.Sprintf("%v", expiresAt))
+	if convErr != nil {
+		return time.Now(), domain.NewError("Error while parsing expiry date: "+convErr.Error(), domain.ERR_UNAUTHORIZED)
+	}
+
+	return expiresAtTime, nil
+}
+
+func (s *JWTService) GetUsername(token *jwt.Token) (string, domain.CodedError) {
+	username, ok := token.Claims.(jwt.MapClaims)["username"]
+	if !ok {
+		return "", domain.NewError("Invalid token: Username not found", domain.ERR_UNAUTHORIZED)
+	}
+
+	return fmt.Sprintf("%v", username), nil
+}
+
+func (s *JWTService) GetRole(token *jwt.Token) (string, domain.CodedError) {
+	role, ok := token.Claims.(jwt.MapClaims)["role"]
+	if !ok {
+		return "", domain.NewError("Invalid token: Role not found", domain.ERR_UNAUTHORIZED)
+	}
+
+	return fmt.Sprintf("%v", role), nil
+}
+
+func (s *JWTService) GetTokenType(token *jwt.Token) (string, domain.CodedError) {
+	tokenType, ok := token.Claims.(jwt.MapClaims)["tokenType"]
+	if !ok {
+		return "", domain.NewError("Invalid token: TokenType not found", domain.ERR_UNAUTHORIZED)
+	}
+
+	return fmt.Sprintf("%v", tokenType), nil
+}
