c87fff4b5b7bc4e6a60cc1c5a9060607e0eaf099
Nathan M.S <natms24@gmail.com>
2024-09-22 22:46:46 +0300

    Add application logic for delete classroom

--- backend/usecase/classroom_usecase.go
+++ backend/usecase/classroom_usecase.go
@@ -1,6 +1,7 @@
 package usecases
 
 import (
+	"context"
 	"learned-api/domain"
 
 	"github.com/gin-gonic/gin"
@@ -24,6 +25,23 @@ func (usecase *ClassroomUsecase) CreateClassroom(c *gin.Context, creatorID strin
 	}
 
 	if err := usecase.repository.CreateClassroom(c, newClassroom); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (usecase *ClassroomUsecase) DeleteClassroom(c context.Context, teacherID string, classroomID string) domain.CodedError {
+	foundClassroom, err := usecase.repository.FindClassroom(c, classroomID)
+	if err != nil {
+		return err
+	}
+
+	if foundClassroom.Owner != teacherID {
+		return domain.NewError(domain.ERR_FORBIDDEN, "only the original owner can delete the classroom")
+	}
+
+	if err = usecase.repository.DeleteClassroom(c, classroomID); err != nil {
 		return err
 	}
 
