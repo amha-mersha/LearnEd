7b52ee70fd1cb2a74855659c4ad92e064867d0cf
Nathan M.S <natms24@gmail.com>
2024-09-22 12:41:45 +0300

    Add domain definitions for classroom

--- backend/domain/domain.go
+++ backend/domain/domain.go
@@ -31,6 +31,38 @@ type User struct {
 	Type     string `json:"type"`
 }
 
+type StudentRecord struct {
+	PostID string `json:"post_id"`
+	Grade  int    `json:"grade"`
+}
+
+type StudentGrade struct {
+	StudentID string          `json:"student_id"`
+	Records   []StudentRecord `json:"records"`
+}
+
+type Post struct {
+	GroupID     string   `json:"group_id"`
+	Content     string   `json:"content"`
+	Files       []string `json:"files"`
+	IsProcessed bool     `json:"is_processed"`
+	// TODO: Add fields for the processed data
+}
+
+type Classroom struct {
+	Name          string         `json:"name"`
+	Teachers      []interface{}  `json:"teachers"`
+	Students      []interface{}  `json:"students"`
+	StudentGrades []StudentGrade `json:"student_grades"`
+	Posts         []Post         `json:"posts"`
+}
+
+type StudyGroup struct {
+	Name     string        `json:"name"`
+	Students []interface{} `json:"students"`
+	Posts    []Post        `json:"posts"`
+}
+
 type AuthUsecase interface {
 	Signup(c *gin.Context, user dtos.SignupDTO) CodedError
 	Login(c *gin.Context, user dtos.LoginDTO) (string, CodedError)
