16cc04a1f8b9c446b69b148d341ee20ec55be932
Sophonias Demiss <sophidemiss@gmail.com>
2024-09-23 11:07:41 +0300

    setup the frontend environment

---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import { redirect } from "next/dist/server/api-utils";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main className="">
+    </main>
+  );
+}
