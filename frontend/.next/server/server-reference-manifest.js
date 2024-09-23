72093d6d07e7407fcac6f6b19ed36d83f161cfc4
Sophonias Demiss <sophidemiss@gmail.com>
2024-09-23 11:12:39 +0300

    setup the frontend environment

--- frontend/.next/server/server-reference-manifest.js
+++ frontend/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"HzIwyQmK/N9sXxpf5yOlBfbyo/ae6V9NKwdS5cHWiQI=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"xiA+laft5WP08wWDef5V6/RRGeRSCJmSSi38wg4wHFQ=\"\n}"
