854bd40709c6e9552e82479cb3038d47a86ce02d
Sophonias Demiss <sophidemiss@gmail.com>
2024-09-24 11:19:08 +0300

    commiting

---
+++ frontend/app/components/Card.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { cardType } from '../types/cardType'
+
+const Card = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Card
--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Sidebar from '../components/Sidebar/Sidebar'
+import { Card } from '@/components/ui/card'
 
 const page = () => {
   return (
-    <div className=' bg-[#F6F6F6] h-[100vh] w-full'>
-      
+    <div className=' bg-[#F6F6F6]'>
+      <div>
+        
+      </div>
     </div>
   )
 }
