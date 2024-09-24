fcbb03f5269e94f9aed0e22425f77612d1303d86
Amha Mersha <amhamersh@gmail.com>
2024-09-24 20:51:06 +0300

    did the ai generate question out of the given pdf DID NOT stored the response in database however

--- backend/infrastructure/ai/ai_service.go
+++ backend/infrastructure/ai/ai_service.go
@@ -2,8 +2,12 @@ package ai_service
 
 import (
 	"context"
+	"fmt"
 	"learned-api/domain"
 	"log"
+	"net/http"
+	"os"
+	"path/filepath"
 	"reflect"
 	"strings"
 
@@ -14,6 +18,11 @@ import (
 type AIService struct {
 	model   domain.AIModelInterface
 	context context.Context
+	client  genai.Client
+}
+
+var AllowedFileTypes = map[string]string{
+	"pdf": "application/pdf",
 }
 
 func NewAIService(context context.Context, apiKey string) *AIService {
@@ -26,6 +35,7 @@ func NewAIService(context context.Context, apiKey string) *AIService {
 	return &AIService{
 		model:   model,
 		context: context,
+		client:  *client,
 	}
 }
 
@@ -57,4 +67,79 @@ func (s *AIService) ExtractText(value interface{}) string {
 		log.Printf("Unsupported type %T for field extraction", value)
 		return ""
 	}
+}
+
+// send file to generate question
+func (s *AIService) GenerateQuestionsFromFile(post domain.Post) domain.CodedError {
+	if err := s.ValidateFile(post.File); err != nil {
+		return err
+	}
+	file, err := s.client.UploadFileFromPath(s.context, post.File, nil)
+	if err != nil {
+		return domain.NewError(fmt.Sprintf("failed to upload file to gemini: %s", err), domain.ERR_INTERNAL_SERVER)
+	}
+	defer s.client.DeleteFile(s.context, file.Name)
+
+	_, err = s.model.GenerateContent(s.context, genai.Text(fmt.Sprintf(`
+        Based on the following content, generate %d multiple-choice questions with 4 choices each. 
+        Also, provide an explanation for the correct answer. Return the result in JSON format.
+		Format: 
+		  [
+        {
+            "question": "Question 1 text",
+            "choices": [
+                "Choice A",
+                "Choice B",
+                "Choice C",
+                "Choice D"
+            ],
+            "correct_answer": "Index of the correct answer",
+            "explanation": "Explanation of why choice A is correct."
+        },
+        {
+            "question": "Question 2 text",
+            "choices": [
+                "Choice A",
+                "Choice B",
+                "Choice C",
+                "Choice D"
+            ],
+            "correct_answer": "Index of the correct answer",
+            "explanation": "Explanation of why choice B is correct."
+        }
+    ]
+    `, 100)),genai.FileData{URI: file.URI})
+	if err != nil{
+		return domain.NewError(fmt.Sprintf("failed to generate questions through gemini: %s", err), domain.ERR_INTERNAL_SERVER)
+	}
+	return nil
+}
+
+func (s *AIService) ValidateFile(filePath string) domain.CodedError {
+	extension := strings.ToLower(filepath.Ext(filePath))
+	extension = extension[1:]
+	if extension == "" {
+		return domain.NewError(fmt.Sprintf("file %s has no extension", extension), domain.ERR_FORBIDDEN)
+	}
+	expectedMimeType, allowed := AllowedFileTypes[extension]
+	if !allowed {
+		return domain.NewError(fmt.Sprintf("file %s is not an allowed type of extension", extension), domain.ERR_FORBIDDEN)
+	}
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		return domain.NewError(fmt.Sprintf("failed to open file: %s", err), domain.ERR_INTERNAL_SERVER)
+	}
+	defer file.Close()
+	buffer := make([]byte, 512)
+	_, err = file.Read(buffer)
+	if err != nil {
+		return domain.NewError(fmt.Sprintf("failed to read the file: %s", err), domain.ERR_INTERNAL_SERVER)
+	}
+
+	mimeType := http.DetectContentType(buffer)
+	if mimeType != expectedMimeType {
+		return domain.NewError(fmt.Sprintf("file type mismatch: expected %s but got %s", expectedMimeType, mimeType), domain.ERR_FORBIDDEN)
+	}
+	return nil
 }
