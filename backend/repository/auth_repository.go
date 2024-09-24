bcecbf37bb27a2674b801920bedde2796187d9da
Nathan M.S <natms24@gmail.com>
2024-09-24 09:35:18 +0300

    Update auth repo functions to use context.Context

--- backend/repository/auth_repository.go
+++ backend/repository/auth_repository.go
@@ -1,10 +1,10 @@
 package repository
 
 import (
+	"context"
 	"learned-api/domain"
 	"strings"
 
-	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -17,7 +17,7 @@ func NewAuthRepository(collection *mongo.Collection) *AuthRepository {
 	return &AuthRepository{collection: collection}
 }
 
-func (r *AuthRepository) CreateUser(c *gin.Context, user domain.User) domain.CodedError {
+func (r *AuthRepository) CreateUser(c context.Context, user domain.User) domain.CodedError {
 	_, err := r.collection.InsertOne(c, user)
 	if mongo.IsDuplicateKeyError(err) && strings.Contains(err.Error(), "email") {
 		return *domain.NewError("an account with that email already exists", domain.ERR_CONFLICT)
@@ -30,7 +30,7 @@ func (r *AuthRepository) CreateUser(c *gin.Context, user domain.User) domain.Cod
 	return nil
 }
 
-func (r *AuthRepository) GetUserByEmail(c *gin.Context, email string) (domain.User, domain.CodedError) {
+func (r *AuthRepository) GetUserByEmail(c context.Context, email string) (domain.User, domain.CodedError) {
 	var foundUser domain.User
 
 	res := r.collection.FindOne(c, bson.D{{Key: "email", Value: email}})
@@ -50,7 +50,7 @@ func (r *AuthRepository) GetUserByEmail(c *gin.Context, email string) (domain.Us
 	return foundUser, nil
 }
 
-func (r *AuthRepository) UpdateUser(c *gin.Context, userEmail string, updateData domain.User) domain.CodedError {
+func (r *AuthRepository) UpdateUser(c context.Context, userEmail string, updateData domain.User) domain.CodedError {
 	updateFields := bson.D{}
 	if updateData.Name != "" {
 		updateFields = append(updateFields, bson.E{Key: "name", Value: updateData.Name})
