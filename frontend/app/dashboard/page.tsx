6d28a5721af82b1d59a444a84b046f5550248d8d
Sophonias Demiss <sophidemiss@gmail.com>
2024-09-24 22:52:05 +0300

    Implemented the class rooms page

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,15 +1,23 @@
-import React from 'react'
-import Sidebar from '../components/Sidebar/Sidebar'
-import { Card } from '@/components/ui/card'
+import React from "react";
+import Sidebar from "../components/Sidebar/Sidebar";
+import Card from "../components/Card";
+import { cardinfo } from "@/utils/carddummy";
 
 const page = () => {
+  const cards = cardinfo;
+
   return (
-    <div className=' bg-[#F6F6F6]'>
-      <div>
-        
+    <div className=" bg-[#F6F6F6] min-h-screen  pr-36 pt-16">
+      <h1 className="text-3xl font-black ml-24">Classes</h1>
+      <div className="  justify-center w-full flex flex-wrap">
+        {cards.map((item, ind) => (
+          <div key={ind} className="w-5/12 ml-8 mt-6">
+            <Card info={cardinfo[ind]} />
+          </div>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
