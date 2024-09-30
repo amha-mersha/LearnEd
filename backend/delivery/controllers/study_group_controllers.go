fe263110eb0c52634c5e5d671d0bb0c807efadd6
Nathan M.S <natms24@gmail.com>
2024-10-01 01:58:55 +0300

    Add handler for delete study group

--- backend/delivery/controllers/study_group_controllers.go
+++ backend/delivery/controllers/study_group_controllers.go
@@ -37,5 +37,28 @@ func (controller *StudyGroupController) CreateStudyGroup(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusCreated, domain.Response{"message": "Classroom created successfully"})
+	c.JSON(http.StatusCreated, domain.Response{"message": "study group created successfully"})
+}
+
+func (controller *StudyGroupController) DeleteStudyGroup(c *gin.Context) {
+	studyGroupID := c.Param("studyGroupID")
+	if studyGroupID == "" {
+		c.JSON(http.StatusBadRequest, domain.Response{"error": "missing study group id"})
+		return
+	}
+
+	id, exists := c.Keys["id"]
+	if !exists {
+		c.JSON(http.StatusForbidden, domain.Response{"message": "coudn't find the id field"})
+		return
+	}
+
+	creatorID := id.(string)
+	err := controller.usecase.DeleteStudyGroup(c, creatorID, studyGroupID)
+	if err != nil {
+		c.JSON(GetHTTPErrorCode(err), domain.Response{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, domain.Response{"message": "study group deleted successfully"})
 }
