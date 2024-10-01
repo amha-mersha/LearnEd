d67b092535295ec863c29fba622cd26f36f51991
Nathan M.S <natms24@gmail.com>
2024-10-01 13:23:02 +0300

    Add endpoints for study group endpoints

--- backend/delivery/routers/study_group_router.go
+++ backend/delivery/routers/study_group_router.go
@@ -13,5 +13,17 @@ func NewStudyGroupRouter(studygroupRep domain.StudyGroupRepository, authReposito
 	sgUsecase := usecases.NewStudyGroupUsecase(studygroupRep, authRepository)
 	sgController := controllers.NewStudyGroupController(sgUsecase)
 
-	router.POST("/", middleware.AuthMiddlewareWithRoles(jwtService, domain.RoleStudent), sgController.CreateStudyGroup)
+	router.POST("/", middleware.AuthMiddlewareWithRoles(jwtService, domain.RoleTeacher), sgController.CreateStudyGroup)
+	router.DELETE("/:studyGroupID", middleware.AuthMiddlewareWithRoles(jwtService, domain.RoleTeacher), sgController.DeleteStudyGroup)
+	router.POST("/:studyGroupID/students", middleware.AuthMiddlewareWithRoles(jwtService, domain.RoleTeacher), sgController.AddStudent)
+	router.DELETE("/:studyGroupID/students/:studentID", middleware.AuthMiddlewareWithRoles(jwtService, domain.RoleTeacher), sgController.RemoveStudent)
+
+	router.POST("/:studyGroupID/posts", middleware.AuthMiddlewareWithRoles(jwtService, domain.RoleTeacher), sgController.AddPost)
+	router.PATCH("/:studyGroupID/posts/:postID", middleware.AuthMiddlewareWithRoles(jwtService, domain.RoleTeacher), sgController.UpdatePost)
+	router.DELETE("/:studyGroupID/posts/:postID", middleware.AuthMiddlewareWithRoles(jwtService, domain.RoleTeacher), sgController.RemovePost)
+
+	router.POST("/:studyGroupID/posts/:postID", middleware.AuthMiddlewareWithRoles(jwtService, domain.RoleTeacher, domain.RoleStudent), sgController.AddComment)
+	router.DELETE("/:studyGroupID/posts/:postID/comments/:commentID", middleware.AuthMiddlewareWithRoles(jwtService, domain.RoleTeacher, domain.RoleStudent), sgController.RemoveComment)
+
+	router.GET("/", middleware.AuthMiddlewareWithRoles(jwtService, domain.RoleStudent, domain.RoleTeacher), sgController.GetClassrooms)
 }
