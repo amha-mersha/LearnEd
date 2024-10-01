6e2134e6409397270ed18592374117e4d7923811
Nathan M.S <natms24@gmail.com>
2024-10-01 03:29:23 +0300

    Add repo functions for find study group

--- backend/repository/study_group_repository.go
+++ backend/repository/study_group_repository.go
@@ -1,6 +1,12 @@
 package repository
 
-import "go.mongodb.org/mongo-driver/mongo"
+import (
+	"context"
+	"learned-api/domain"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 type StudyGroupRepository struct {
 	collection *mongo.Collection
@@ -10,4 +16,28 @@ func NewStudyGroupRepository(collection *mongo.Collection) *StudyGroupRepository
 	return &StudyGroupRepository{
 		collection: collection,
 	}
+}
+
+func (repository *ClassroomRepository) FindStudyGroup(c context.Context, studyGroupID string) (domain.StudyGroup, domain.CodedError) {
+	var studyGroup domain.StudyGroup
+	id, pErr := repository.ParseID(studyGroupID)
+	if pErr != nil {
+		return studyGroup, pErr
+	}
+
+	res := repository.collection.FindOne(c, bson.D{{Key: "_id", Value: id}})
+	if res.Err() == mongo.ErrNoDocuments {
+		return studyGroup, domain.NewError("study group not found", domain.ERR_NOT_FOUND)
+	}
+
+	if res.Err() != nil {
+		return studyGroup, domain.NewError(res.Err().Error(), domain.ERR_INTERNAL_SERVER)
+	}
+
+	err := res.Decode(&studyGroup)
+	if err != nil {
+		return studyGroup, domain.NewError(err.Error(), domain.ERR_INTERNAL_SERVER)
+	}
+
+	return studyGroup, nil
 }
